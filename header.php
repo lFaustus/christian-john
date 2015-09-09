d81f6f925c2ed0759aec5931680bd5ece80f9575
markensteinv2 <heyimezekiel@gmail.com>
2015-09-09 19:06:30 +0800

    First commit

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
