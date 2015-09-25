5db32b65916137396add7435da91de38165284d0
markensteinv2 <heyimezekiel@gmail.com>
2015-09-25 23:06:25 +0800

    Mobile responsive

--- enduser/listrequirement.php
+++ enduser/listrequirement.php
@@ -147,6 +147,12 @@ $message="No REQUIREMENTS";
  </div>
 
 
+    <!-- jQuery -->
+    <script src="../js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../js/bootstrap.min.js"></script>
+
 
 
 
